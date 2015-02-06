39a41cb3bed50f3df5f65d5f0df7fa918e2bc3d0
Christos Vasilakis <cvasilak@gmail.com>
2015-02-06 12:55:56 +0200

    create target AeroGearSync-JSONPatch

---
+++ AeroGearSync-JSONPatch/AeroGearSync-JSONPatch-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  AeroGearSync-JSONPatch-Bridging-Header.h
+//  AeroGearSync
+//
+//  Created by Christos Vasilakis on 2/6/15.
+//  Copyright (c) 2015 aerogear. All rights reserved.
+//
+
+#ifndef AeroGearSync_AeroGearSync_JSONPatch_Bridging_Header_h
+#define AeroGearSync_AeroGearSync_JSONPatch_Bridging_Header_h
+
+#import <JSONTools/JSONTools.h>
+
+#endif
---
+++ AeroGearSync-JSONPatch/JsonPatchEdit.swift
@@ -0,0 +1,26 @@
+public struct JsonPatchEdit: Edit {
+    public let clientId: String
+    public let documentId: String
+    public let clientVersion: Int
+    public let serverVersion: Int
+    public let checksum: String
+    public let diffs: [JsonPatchDiff]
+    
+    public init(clientId: String, documentId: String, clientVersion: Int, serverVersion: Int, checksum: String, diffs: [JsonPatchDiff]) {
+        self.clientId = clientId
+        self.documentId = documentId
+        self.clientVersion = clientVersion
+        self.serverVersion = serverVersion
+        self.checksum = checksum
+        self.diffs = diffs
+    }
+}
+
+public func ==(lhs: JsonPatchEdit, rhs: JsonPatchEdit) -> Bool {
+    return lhs.clientId == rhs.clientId &&
+        lhs.documentId == rhs.documentId &&
+        lhs.serverVersion == rhs.serverVersion &&
+        lhs.checksum == rhs.checksum &&
+        lhs.diffs.count == rhs.diffs.count
+}
+
