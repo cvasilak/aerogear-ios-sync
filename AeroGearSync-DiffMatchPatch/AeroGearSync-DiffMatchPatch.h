244a9a60cb6ba31df100b4ebb2f931489572a434
Christos Vasilakis <cvasilak@gmail.com>
2015-02-06 12:55:01 +0200

    create target AeroGearSync-DiffMatchPatch

---
+++ AeroGearSync-DiffMatchPatch/AeroGearSync-DiffMatchPatch.h
@@ -0,0 +1,19 @@
+//
+//  AeroGearSync-DiffMatchPatch.h
+//  AeroGearSync-DiffMatchPatch
+//
+//  Created by Christos Vasilakis on 2/6/15.
+//  Copyright (c) 2015 aerogear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AeroGearSync-DiffMatchPatch.
+FOUNDATION_EXPORT double AeroGearSync_DiffMatchPatchVersionNumber;
+
+//! Project version string for AeroGearSync-DiffMatchPatch.
+FOUNDATION_EXPORT const unsigned char AeroGearSync_DiffMatchPatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AeroGearSync_DiffMatchPatch/PublicHeader.h>
+
+
---
+++ AeroGearSync-DiffMatchPatch/DiffMatchPatchDiff.swift
@@ -0,0 +1,17 @@
+
+public struct DiffMatchPatchDiff: Difference {
+    
+    public let operation: Operation
+    public let text: String
+    
+    public enum Operation : String {
+        case Add = "ADD"
+        case Delete = "DELETE"
+        case Unchanged = "UNCHANGED"
+    }
+    
+    public init(operation: Operation, text: String) {
+        self.operation = operation
+        self.text = text
+    }
+}
