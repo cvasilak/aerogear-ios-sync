a7ed1d06b8acb5ee2e7fe623af81ed1d3b2aec7d
Christos Vasilakis <cvasilak@gmail.com>
2015-02-13 12:28:31 +0200

    introduce DiffMatchPatchMessage

---
+++ AeroGearSync-DiffMatchPatch/DiffMatchPatchMessage.swift
@@ -0,0 +1,88 @@
+//
+//  JsonPatchMessage.swift
+//  AeroGearSyncClient
+//
+//  Created by Christos Vasilakis on 2/12/15.
+//  Copyright (c) 2015 aerogear. All rights reserved.
+//
+
+import Foundation
+import AeroGearSync
+
+public class DiffMatchPatchMessage<E>:PatchMessage<DiffMatchPatchEdit>, Printable {
+    
+    public override init() {
+        super.init()
+    }    
+    
+    override public func asJson() -> String {
+        // TODO: This should be solved on the server side.
+        var str = "{\"msgType\":\"patch\",\"id\":\"" + documentId + "\",\"clientId\":\"" + clientId + "\""
+        str += ",\"edits\":["
+        let count = edits.count-1
+        for i in 0...count {
+            let edit = edits[i]
+            str += "{\"clientVersion\":\(edit.clientVersion)"
+            str += ",\"serverVersion\":\(edit.serverVersion)"
+            str += ",\"checksum\":\"\(edit.checksum)"
+            str += "\",\"diffs\":["
+            let diffscount = edit.diffs.count-1
+            for y in 0...diffscount {
+                let text = edit.diffs[y].text.stringByReplacingOccurrencesOfString("\"", withString: "\\\"", options: NSStringCompareOptions.LiteralSearch, range: nil)
+                str += "{\"operation\":\"" + edit.diffs[y].operation.rawValue + "\",\"text\":\"" + text + "\"}"
+                if y != diffscount {
+                    str += ","
+                }
+            }
+            str += "]}]}"
+        }
+        return str
+    }
+    
+    override public func fromJson(var json:String) -> PatchMessage<DiffMatchPatchEdit>? {
+        if let dict = asDictionary(json) {
+            let id = dict["id"] as String
+            let clientId = dict["clientId"] as String
+            var edits = Array<DiffMatchPatchEdit>()
+            for (key: String, jsonEdit) in dict["edits"] as [String: AnyObject] {
+                var diffs = Array<DiffMatchPatchDiff>()
+                for (key: String, jsonDiff) in jsonEdit["diffs"] as [String: AnyObject] {
+                    diffs.append(DiffMatchPatchDiff(operation: DiffMatchPatchDiff.Operation(rawValue: jsonDiff["operation"] as String)!,
+                       text: jsonDiff["text"] as String))
+                }
+                edits.append(DiffMatchPatchEdit(clientId: clientId,
+                    documentId: id,
+                    clientVersion: jsonEdit["clientVersion"] as Int,
+                    serverVersion: jsonEdit["serverVersion"] as Int,
+                    checksum: jsonEdit["checksum"] as String,
+                    diffs: diffs))
+            }
+            return PatchMessage(id: id, clientId: clientId, edits: edits)
+        }
+        return Optional.None
+    }
+    
+    /**
+    Tries to convert the passed in String into a Swift Dictionary<String, AnyObject>
+    
+    :param: jsonString the JSON string to convert into a Dictionary
+    :returns: Optional Dictionary<String, AnyObject>
+    */
+    public func asDictionary(jsonString: String) -> Dictionary<String, AnyObject>? {
+        var jsonErrorOptional: NSError?
+        return NSJSONSerialization.JSONObjectWithData((jsonString as NSString).dataUsingEncoding(NSUTF8StringEncoding)!,
+            options: NSJSONReadingOptions(0), error: &jsonErrorOptional) as? Dictionary<String, AnyObject>
+    }
+    
+    /**
+    Tries to convert the passed in Dictionary<String, AnyObject> into a JSON String representation.
+    
+    :param: the Dictionary<String, AnyObject> to try to convert.
+    :returns: optionally the JSON string representation for the dictionary.
+    */
+    public func asJsonString(dict: Dictionary<String, AnyObject>) -> String? {
+        var jsonErrorOptional: NSError?
+        var data = NSJSONSerialization.dataWithJSONObject(dict, options:NSJSONWritingOptions(0), error: &jsonErrorOptional)
+        return NSString(data: data!, encoding: NSUTF8StringEncoding)
+    }
+}
